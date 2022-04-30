9b26e145a060f359bda13bcaf0a4e30f8d60d8f6
De_Jong <johann.de_jong@boehringer-ingelheim.com>
2022-04-30 15:01:01 +0200

    Re-organized the code

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='VaDER',
+    version='0.1.0',
+    author='Johann de Jong',
+    author_email='johanndejong@gmail.com',
+    packages=['VaDER'],
+    #    package_dir={'': '.'},
+    #    scripts=['bin/script1', 'bin/script2'],
+    url='',
+    license='LICENSE',
+    description='Clustering multivariate time series with missing values',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy >= 1.19.5",
+        "scikit_learn >= 1.0.2",
+        "scipy >= 1.7.3",
+        "setuptools >= 57.0.0",
+        "tensorflow >= 2.4.1",
+        "tensorflow_addons >= 0.16.1"
+    ]
+)
